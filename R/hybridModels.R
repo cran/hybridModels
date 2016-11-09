85a1bd87eb00bfe32ed569f233dfd751091f498f
Fernando S. Marques <fernandosix@gmail.com>
2016-11-09 00:34:28 +0000

    version 0.2.9

--- R/hybridModels.R
+++ R/hybridModels.R
@@ -1,12 +1,12 @@
-#' hybridModels: A package for simulating hybrid models in dynamics networks.
+#' hybridModels: an R package for stochastic simulation of disease spreading in dynamic networks.
 #'
 #' The hybridModels package provides functions to simulate stochastics models
 #' in dynamics networks, using two processes in different scales:
 #' 1 Global scale to simulate the transmission from one node to another
 #' 2 Local sacale to simulate the transmission inside the node
 #' 
-#' @section The models are:
-#' 'SI model without demographics'
+#' @section Modelling transmission of diseases:
+#' 'Framework to run n simulations in dynamic network and plot results'
 #'
 #' @docType package
 #' @name hybridModels
